a40d00b3e7f3fd4b27ef4441c2d48edb9939e25d
dgarg <dgarg@example.com>
2026-07-13 19:06:49 +0530

    chore: add Render deployment config (Procfile, render.yaml, gunicorn)

--- app.py
+++ app.py
@@ -246,4 +246,6 @@ def api_practice():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(debug=debug, host='0.0.0.0', port=port)
